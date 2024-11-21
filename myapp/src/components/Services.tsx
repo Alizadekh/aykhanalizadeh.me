c31d543af69bfa3aa5abbcfd151bf5d310bad6f7
Alizadekh <aykhanalizadekh@gmail.com>
2024-11-21 20:55:54 +0000

    Adding Services page and fixing some bugs in intro

--- myapp/src/components/Services.tsx
+++ myapp/src/components/Services.tsx
@@ -1,16 +1,41 @@
 import style from "../style/Services.module.css";
+import Shopping from "../assets/img/RestaurantServiceImg.png";
+import CRM from "../assets/img/CRMServiceImg.png";
+import Landing from "../assets/img/LandingServiceImg.png";
 
 function Services() {
   return (
     <section id="services" className={style.services}>
-      <div className="info">
-        <h2>
-          My <span>Services</span>
-        </h2>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis lacus
-          nunc, posuere in justo vulputate, bibendum sodales
-        </p>
+      <div className={style.servicesContent}>
+        <div className={style.info}>
+          <h2>
+            My <span>Services</span>
+          </h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis lacus
+            nunc, posuere in justo vulputate, bibendum sodales
+          </p>
+        </div>
+        <div className={style.serviceBoxes}>
+          <div>
+            <div>
+              <h3>Restaurant\Shopping Site</h3>
+            </div>
+            <img src={Shopping} alt="" />
+          </div>
+          <div>
+            <div>
+              <h3>CRM App</h3>
+            </div>
+            <img src={CRM} alt="" />
+          </div>
+          <div>
+            <div>
+              <h3>Landing Page</h3>
+            </div>
+            <img src={Landing} alt="" />
+          </div>
+        </div>
       </div>
     </section>
   );
--- myapp/src/pages/Home.tsx
+++ myapp/src/pages/Home.tsx
@@ -1,9 +1,11 @@
 import Intro from "../components/Intro";
+import Services from "../components/Services";
 
 function Home() {
   return (
     <div>
       <Intro />
+      <Services />
     </div>
   );
 }
