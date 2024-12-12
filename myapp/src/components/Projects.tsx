24f1e40c4b9dc39abe68a5f56b6928c6de65f5d3
Alizadekh <aykhanalizadekh@gmail.com>
2024-12-12 18:13:23 +0000

    Extract projects data from projects tsx and fixing bugs

--- myapp/src/components/Projects.tsx
+++ myapp/src/components/Projects.tsx
@@ -1,98 +1,12 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Pagination } from "swiper/modules";
+import { projectDetails } from "../data/projects";
 import "../../node_modules/swiper/swiper-bundle.min.css";
 import style from "../style/Projects.module.css";
 
-import Foody from "../assets/img/Foody.png";
-import MashaLaunge from "../assets/img/MashaLaunge.png";
-import WeatherApp from "../assets/img/WeatherApp.png";
-import TodoApp from "../assets/img/TodoProject.png";
-
-interface ProjectDetails {
-  projectName: string;
-  imgPath: string;
-  description: string;
-  usedTools: string[];
-  repoLink: string;
-  demoLink: string;
-}
-
-const projectDetails: ProjectDetails[] = [
-  {
-    projectName: "Foody",
-    imgPath: Foody,
-    description:
-      '"Foody" is a multifunctional project that combines food ordering, order taking, cart functionality, payment system, and "admin panel" logic for restaurants.',
-    usedTools: [
-      "React",
-      "JavaScript",
-      "Material UI",
-      "Swiper JS",
-      "Axios",
-      "Firebase",
-      "FireStore",
-      "React-Toastify",
-      "React-Router-Dom",
-      "React-Bootstrap",
-    ],
-    repoLink: "https://github.com/Alizadekh/Food-admin",
-    demoLink: "Not available",
-  },
-  {
-    projectName: "Restaurant CRM",
-    imgPath: MashaLaunge,
-    description:
-      "This control panel is designed for restaurant chains. Through the panel, the restaurant owner can view, analyze, and edit all the processes taking place in his business: orders, payments, customer information, checks, employee information, and in-restaurant expenses in real time.",
-    usedTools: [
-      "React",
-      "JavaScript",
-      "Material UI",
-      "Axios",
-      "React-Toastify",
-      "React-Router-Dom",
-      "React-Bootstrap",
-    ],
-    repoLink: "Not available",
-    demoLink: "Not available",
-  },
-  {
-    projectName: "Weather App (Demo)",
-    imgPath: WeatherApp,
-    description:
-      "If you need information about the weather in your area, you can use this website to type in the name of your city and see current, daily, and hourly weather information. You can also get instant weather information for famous cities around the world.",
-    usedTools: [
-      "React",
-      "JavaScript",
-      "Axios",
-      "Weather API",
-      "Google Maps API",
-      "React-Router-Dom",
-    ],
-    repoLink: "https://github.com/Alizadekh/Weather-App-React",
-    demoLink: "https://weather-app-react-qk6p.vercel.app/",
-  },
-  {
-    projectName: "To-Do App (Demo)",
-    imgPath: TodoApp,
-    description:
-      "A To-Do App that works with simple logic. Through the app, you can schedule any task for any time you want and see the remaining time for your scheduled task. It also has the functionality to delete what you have completed from the list.",
-    usedTools: [
-      "React",
-      "JavaScript",
-      "LocalStorage",
-      "React-Toastify",
-      "Material UI",
-    ],
-    repoLink: "https://github.com/Alizadekh/Weather-App-React",
-    demoLink: "https://github.com/Alizadekh/To-Do-Application",
-  },
-];
-
-function Projects() {
-  const [selectedProject, setSelectedProject] = useState<ProjectDetails>(
-    projectDetails[0]
-  );
+const Projects: React.FC = () => {
+  const [selectedProject, setSelectedProject] = useState(projectDetails[0]);
 
   const handleSlideChange = (index: number) => {
     setSelectedProject(projectDetails[index]);
@@ -176,6 +90,6 @@ function Projects() {
       </div>
     </section>
   );
-}
+};
 
 export default Projects;
