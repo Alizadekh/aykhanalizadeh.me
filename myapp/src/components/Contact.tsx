486e6a0f0f4fe450731b8d3b77e6498a0130d2db
Alizadekh <aykhanalizadekh@gmail.com>
2024-12-13 23:45:42 +0000

    Creating new contact section and add new submit technology

--- myapp/src/components/Contact.tsx
+++ myapp/src/components/Contact.tsx
@@ -1,16 +1,57 @@
+import React, { useState } from "react";
+import { useForm, ValidationError } from "@formspree/react";
 import style from "../style/Contact.module.css";
-
-import skills from "../data/skills";
+import smsIcon from "../assets/decorations/sms.svg";
 
 const Contact: React.FC = () => {
+  const [state, handleSubmit] = useForm("xbljevor");
+  const [isFormSubmitted, setIsFormSubmitted] = useState(false);
+
+  const handleFormSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    handleSubmit(event).then(() => {
+      setIsFormSubmitted(true);
+    });
+  };
+
   return (
-    <section id="contact">
-      <div className={style.marqueeContainer}>
-        <div className={style.marquee}>
-          {Object.values(skills).map((skill, index) => (
-            <span key={index}>{skill}</span>
-          ))}
-        </div>
+    <section id="contact" className={style.contactSection}>
+      <div className={style.contactContent}>
+        <h2>
+          Have an Awesome Project Idea? <span>Let’s Discuss</span>
+        </h2>
+        {isFormSubmitted ? (
+          <p className={style.successMessage}>
+            Thank you! I'll be in touch soon.
+          </p>
+        ) : (
+          <form onSubmit={handleFormSubmit} className={style.form}>
+            <div>
+              <img src={smsIcon} alt="Email Icon" />
+            </div>
+            <input
+              id="email"
+              type="email"
+              name="email"
+              placeholder="Enter Your Email"
+              title="input"
+              className={style.input}
+              required
+            />
+            <ValidationError
+              prefix="Email"
+              field="email"
+              errors={state.errors}
+            />
+            <button
+              type="submit"
+              title="button"
+              disabled={state.submitting}
+              className={style.button}
+            >
+              Send
+            </button>
+          </form>
+        )}
       </div>
     </section>
   );
