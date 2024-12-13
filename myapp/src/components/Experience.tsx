211c672a8bf6d2a0940836ebf3cbb5f65ac496f3
Alizadekh <aykhanalizadekh@gmail.com>
2024-12-13 21:13:38 +0000

    Extract experiences from component and add to data

--- myapp/src/components/Experience.tsx
+++ myapp/src/components/Experience.tsx
@@ -1,50 +1,8 @@
 import React from "react";
 import style from "../style/Experience.module.css";
-
-type ExperienceType = {
-  title: string;
-  type: string;
-  company: string;
-  date: string;
-  myTasks: string[];
-};
+import myExperiences from "../data/experiences";
 
 const Experience: React.FC = () => {
-  const myExperiences: ExperienceType[] = [
-    {
-      title: "Front End Developer",
-      type: "Remote",
-      company: "Masha Launge",
-      date: "08.2024 - 11.2024",
-      myTasks: [
-        "Developed a custom CRM system for the restaurant using React and Material UI.",
-        "Enhanced user experience and streamlined customer management processes.",
-        "Worked closely with stakeholders to ensure the system met specific business objectives and continuously optimized performance.",
-      ],
-    },
-    {
-      title: "Front End Mentor",
-      type: "Intern",
-      company: "Baku American Center",
-      date: "10.2023 - 01.2024",
-      myTasks: [
-        "Provided targeted instruction in Front-End Programming focusing on key technologies and foundational concepts.",
-        "Developed weekly lesson plans to ensure students mastered essential skills.",
-        "Maintained communication with management to report progress and align with institutional goals.",
-      ],
-    },
-    {
-      title: "Graphics Designer",
-      type: "Intern",
-      company: "Destech Training and Consulting",
-      date: "07.2023 - 09.2023",
-      myTasks: [
-        "Designed and developed social media posts and promotional materials, leveraging strong graphic design skills.",
-        "Collaborated with the team to create impactful visual content to enhance the firm's online presence.",
-      ],
-    },
-  ];
-
   return (
     <section id="about" className={style.experienceSection}>
       <div className={style.experienceContent}>
---
+++ myapp/src/data/experiences.ts
@@ -0,0 +1,36 @@
+const myExperiences = [
+  {
+    title: "Front End Developer",
+    type: "Remote",
+    company: "Masha Launge",
+    date: "08.2024 - 11.2024",
+    myTasks: [
+      "Developed a custom CRM system for the restaurant using React and Material UI.",
+      "Enhanced user experience and streamlined customer management processes.",
+      "Worked closely with stakeholders to ensure the system met specific business objectives and continuously optimized performance.",
+    ],
+  },
+  {
+    title: "Front End Mentor",
+    type: "Intern",
+    company: "Baku American Center",
+    date: "10.2023 - 01.2024",
+    myTasks: [
+      "Provided targeted instruction in Front-End Programming focusing on key technologies and foundational concepts.",
+      "Developed weekly lesson plans to ensure students mastered essential skills.",
+      "Maintained communication with management to report progress and align with institutional goals.",
+    ],
+  },
+  {
+    title: "Graphics Designer",
+    type: "Intern",
+    company: "Destech Training and Consulting",
+    date: "07.2023 - 09.2023",
+    myTasks: [
+      "Designed and developed social media posts and promotional materials, leveraging strong graphic design skills.",
+      "Collaborated with the team to create impactful visual content to enhance the firm's online presence.",
+    ],
+  },
+];
+
+export default myExperiences;
